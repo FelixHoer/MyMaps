92712f6d9a926bde10f9ec0efec51080bb2c9148
Felix Hoerandner <felix.hoerandner@gmx.at>
2012-02-27 16:49:00 +0100

    cleaned up data.js, not needed anymore moved description of json-structure of import/export to map.js

--- js/map.js
+++ js/map.js
@@ -119,6 +119,32 @@ var MM = (function(){
   };
   
   var loadData = function(callback){
+    /*
+      the callback expects a layer object as only parameter
+    
+      component: is abstract
+        type: predefined string (layer | marker | polyline | ...)
+        name: string
+        visible: boolean (if undefined assumed true)
+      
+      layer: is a component
+        items: component[]
+        open: boolean
+      
+      marker: is a component
+        position: position
+        address: string (optional)
+      
+      polyline: is a component
+        items: waypoint[]
+      
+      waypoint: is a component
+        position: position
+    
+      position:
+        lat: num
+        lng: num
+    */
     
     var loadDialog = $('#loadTemplate').tmpl().dialog({
       title: 'MyMaps', 
